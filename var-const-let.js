fe673d78d09ac44bf0d65c9e43333ae522a588b1
Pushkar Dangi <pushkardangi746@gmail.com>
2024-05-23 23:55:12 +0530

    added: var, let and const

---
+++ var-const-let.js
@@ -0,0 +1,10 @@
+let name = "Pushkar"
+var pswrd = "12345@pw"
+const username = "pushkardangi"
+account = 343434
+
+let something;
+
+console.table([name, pswrd, username, account, something])
+
+// Use only Let & Const - they support block scope
